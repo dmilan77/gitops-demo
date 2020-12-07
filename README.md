ba4ab981310a3141f0e6cb437c506c913041677f
Milan Das <milan@ulap.co>
2020-12-06 20:02:14 -0500

    with gitignore

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gitops-demo
+```
+helm repo add argo https://argoproj.github.io/argo-helm
+helm upgrade --install argo-cd argo/argo-cd -f argo-cd/values.yaml --namespace  argo-cd --create-namespace
+```
--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,20 +1,8 @@
-provider "google" {
-  version = "~> 3.49.0"
-  region  = "us-central1"
-  # credentials = file("/xxxxx/xxxx/xxxx/account.json")
-}
-terraform {
-  backend "gcs" {
-    bucket  = "tfstate-data-protection-01"
-    prefix  = "terraform/state/gke/gitops-demo"
-  }
-}
-
 resource "google_container_cluster" "primary" {
   name               = "gitops-demo"
-  project             = "data-protection-01"
   location           = "us-central1-a"
   initial_node_count = 3
+  network   = google_compute_subnetwork.gitops_gke_subnetwork_us_central1.id
 
   master_auth {
     username = ""
